6d7c157d6e39ae2c3f7d0d6258f88ba056e371d4
Steffen Hiller <shiller@local.(none)>
2008-04-25 22:15:57 -0400

    First working version, yeah.

--- lib/active_attributes.rb
+++ lib/active_attributes.rb
@@ -1,1 +1,59 @@
-# ActiveAttributes
+module ActiveAttributes #:nodoc:
+  
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    
+    def attr_order(*attributes)
+      class << self
+        attr_accessor :original_ordered_attributes
+
+        def ordered_attributes(custom_attributes = nil)
+          ordered_attributes = custom_attributes || self.original_ordered_attributes
+          attributes = []
+          ordered_attributes.each do |attr|
+            if childs = self.attribute_childs(attr)
+              if childs.is_a? Array
+                childs.each do |child|
+                  attributes << child
+                end
+              else
+                attributes << childs
+              end
+            end
+          end
+          attributes
+        end
+        
+        def attribute_childs(attribute)
+          if self.attribute_groups
+            if childs = self.attribute_groups[attribute]
+              return childs
+            end
+          end
+          attribute
+        end
+      end
+
+      self.original_ordered_attributes = attributes
+      
+    end
+    
+    def attr_groups(groups = {})
+      class << self
+        attr_accessor :attribute_groups
+      end
+
+      self.attribute_groups = groups
+      
+    end
+    
+    def attr_from(groups = {})
+    end
+
+  end
+end
+
+ActiveRecord::Base.send :include, ActiveAttributes
